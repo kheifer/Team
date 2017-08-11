9defcc1b782a32377b58525ff92d547e1cf01263
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-11 11:03:26 -0700

    add third test for multiple content

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Team {
     private String teamName;
     private String teamMember;
+    private static ArrayList<Team> team = new ArrayList<Team>();
 
     public Team(String teamName, String teamMember){
         this.teamName = teamName;
@@ -15,4 +18,8 @@ public class Team {
     public String getTeamMember() {
         return teamMember;
     }
+    public static ArrayList<Team> getAll() {
+        return team;
+    }
+
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -13,6 +13,7 @@ public class TeamTest {
 
     @After
     public void tearDown() throws Exception {
+
     }
     @Test
     public void NewTestObjectGetsCorrectlyCreated_true_2() throws Exception {
@@ -24,7 +25,13 @@ public class TeamTest {
         Team testTeam = setupTeam();
         assertEquals("Cobras", testTeam.getTeamName());
     }
-
+    @Test
+    public void all_returnsAllInstancesOfTeam_true() {
+        Team testTeam = setupTeam();
+        Team secondTeam = new Team("TeamTwo", "Dagger");
+        assertEquals(true, Team.getAll().contains(testTeam));
+        assertEquals(true, Team.getAll().contains(secondTeam));
+    }
 
 
     public Team setupTeam(){
