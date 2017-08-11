1bcf8826b333f108fd992dc5a4fe37f3af5f707d
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-11 10:20:10 -0700

    add first test

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,13 @@ public class TeamTest {
     @After
     public void tearDown() throws Exception {
     }
+    @Test
+    public void NewTestObjectGetsCorrectlyCreated_true() throws Exception {
+        Team testTeam = setupTeam();
+        assertEquals(true, testTeam instanceof Team);
+    }
 
+    public Team setupTeam(){
+        return new Team ();
+    }
 }
