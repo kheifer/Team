48ca709100c448964ebe4df967971b40bbaf61f9
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-18 09:04:42 -0700

    edit Memberdao interface

--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -1,4 +1,22 @@
 package dao;
 
-public class MemberDao {
+import models.Member;
+
+import java.util.List;
+
+public interface MemberDao {
+
+    void add(Member member);
+
+    Member findById(int id);
+
+    List<Member> getAll();
+
+    void update(String name, String homeTown, String occupation, int age, int id, int memberId);
+
+    void deleteByID(int id);
+
+    void deleteAllMembers();
+
+
 }
