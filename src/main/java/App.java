ba8e4fcf4385fb4c7019d8c1cb74f7dfa3805fc9
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-18 11:44:39 -0700

    add RESTful routes to process new team form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,42 +1,59 @@
-//import static spark.Spark.*;
-//import spark.ModelAndView;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//import models.Team;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class App {
-//    public static void main(String[] args) {
-//        staticFileLocation("/public");
-//
-//        //get: homepage/displays all teams
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            model.put("teams", Team.getAll());
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        //get: show form to enter new post
-//        get("/teams/new",(request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model,"newTeam-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        //post: process new post form
-//        post("/teams/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            ArrayList<Team> listOfTeams = Team.getAll();
-//            String teamName = request.queryParams("teamName");
-//            String teamDescription = request.queryParams("teamDescription");
-//            Team newTeam = new Team(teamName,teamDescription);
-//            model.put("teams", listOfTeams);
-//            return new ModelAndView(model,"index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        //get: show detailed information about a restaurant
+import static spark.Spark.*;
+
+import dao.Sql2oMemberDao;
+import dao.Sql2oTeamDao;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import models.Team;
+import models.Member;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.staticFileLocation;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/team.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oMemberDao memberDao = new Sql2oMemberDao(sql2o);
+        Sql2oTeamDao teamDao = new Sql2oTeamDao(sql2o);
+
+        //get: homepage/displays all teams
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<Team> teamList = teamDao.getAll();
+            List<Member> memberList = memberDao.getAll();
+            model.put("teams", teamList);
+            model.put("members", memberList);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+//        //get: show form to enter new team
+        get("/teams/new",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Team> teamList = teamDao.getAll();
+            model.put("teams", teamList);
+            return new ModelAndView(model,"newTeam-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process new team form
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String teamName = request.queryParams("teamName");
+            String teamDescription = request.queryParams("teamDescription");
+            Team newTeam = new Team(teamName,teamDescription);
+            teamDao.add(newTeam);
+            List<Team> teamList = teamDao.getAll();
+            model.put("teams", teamList);
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: show detailed information about a restaurant
 //        get("/teams/:id", (request, response) -> {
 //            Map<String, Object> model = new HashMap<String, Object>();
 //            int idOfTeam = Integer.parseInt(request.params("id"));
-//            Team newTeam = Team.findById(idOfTeam);
+//            List<Member>
 //            ArrayList<String> newMembers = newTeam.getMembers();
 //            model.put("newTeam", newTeam);
 //            model.put("newMembers", newMembers);
@@ -86,5 +103,5 @@
 //            model.put("teams", listOfTeams);
 //            return new ModelAndView(model,"index.hbs");
 //        }, new HandlebarsTemplateEngine());
-//    }
-//}
+    }
+}
