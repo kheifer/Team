2f030e4fc6a4aa0db63136a07fe10c8db7c179fd
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-18 09:28:07 -0700

    fail add test for TeamDao

--- src/test/java/dao/Sql2oTeamDaoTest.java
+++ src/test/java/dao/Sql2oTeamDaoTest.java
@@ -1,22 +1,40 @@
 package dao;
 
+import models.Team;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oTeamDaoTest {
+    private Sql2oTeamDao teamDao;
+    private Sql2oMemberDao memberDao;
+    private Connection con;
+
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString,"","");
+        teamDao =  new Sql2oTeamDao(sql2o);
+        memberDao = new Sql2oMemberDao(sql2o);
+
+        con = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        con.close();
     }
 
     @Test
-    public void add() throws Exception {
+    public void teamAddsNewInstanceWithId() throws Exception {
+        Team team = setNewTeam();
+        teamDao.add(team);
+        assertEquals(1, team.getId());
+
     }
 
     @Test
@@ -43,4 +61,7 @@ public class Sql2oTeamDaoTest {
     public void deleteById() throws Exception {
     }
 
+    public Team setNewTeam(){
+        return new Team("Warriors","we wear cool red jackets");
+    }
 }
