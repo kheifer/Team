3f41c2d74878fdac289a84bf91b6504092cd7138
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-18 08:57:29 -0700

    add methods to TeamDao interface

--- src/main/java/dao/TeamDao.java
+++ src/main/java/dao/TeamDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Member;
+import models.Team;
+
+import java.util.List;
+
 public interface TeamDao {
+
+    void add(Team team);
+
+    List<Team> getAll();
+
+    List<Member> getAllMembersByTeamId(int id);
+
+    Team findById(int id);
+
+    void update(String name, String description, int id);
+
+    void deleteAllTeams();
+
+    void deleteById(int id);
+
 }
